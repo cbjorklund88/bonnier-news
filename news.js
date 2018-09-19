25e08d7c0196159d116f17bda92bd2b7a76185da
SaraNordell <sara@nordell.me>
2018-09-19 15:34:32 +0200

    Wednesday 15:34

--- news.js
+++ news.js
@@ -1,49 +1,35 @@
 // Write your Javascript excercises here:
 // Our variables
 const key = "8cc94279432f44f38ef448ffa067b76c"
-const urldetox = `https://newsapi.org/v2/everything?q=Digital-detox&language=en&apiKey=${key}`
-const urlDigital = `https://newsapi.org/v2/everything?q=Digital&language=en&apiKey=${key}`
+const urlNewsBox = `https://newsapi.org/v2/everything?q=Stockholm&konst&kultur&language=sv&sortBy=publishedAt&apiKey=${key}`
+const urlSideBar = `https://newsapi.org/v2/everything?q=Stockholm&konst&kultur&language=sv&sortBy=publishedAt&apiKey=${key}`
+const urlheadnews = `https://newsapi.org/v2/everything?q=Stockholm&konst&kultur&language=sv&sortBy=publishedAt&apiKey=${key}`
+const urlpopularSideBar = `https://newsapi.org/v2/everything?q=Stockholm&konst&kultur&language=sv&sortBy=popularity&apiKey=${key}`
 
-// Our main function
-const recievedNews = (newsdata) => {
+
+// Our function headnews div
+const recievedNewsheadnews = (newsdata) => {
 
 	// For each article object from the API, we create a new div in HTML.
-    newsdata.articles.forEach((article) => {
-      const descriptionLength = 98;
-      let shortDescription = article.description;
-      if (article.description.length > descriptionLength) {
-        shortDescription = article.description.substring (0,descriptionLength)+'...';
+    newsdata.articles.forEach((article, index) =>{
+
+      if(article.urlToImage && index < 1) {
+
+        //Here we create and add html elements to our html file
+        document.querySelector(".headnews").innerHTML +=
+          `<div class="headnews-news">
+              <a href ="${article.url}" target="_blank">
+                <h3>${article.title}</h3>
+                <img src="${article.urlToImage}"/>
+              </a>
+            </div>`
       }
-
-      if(article.urlToImage) {
-
-					//Here we create and add html elements to our html file
-					document.querySelector(".newsBox").innerHTML +=
-            `<div class="newsBox-news">
-                <a href ="${article.url}" target="_blank">
-                  <h6>${article.publishedAt}</h6>
-                  <h2>${article.title}</h2>
-                  <h5>${article.source.name}</h5>
-                  <img src="${article.urlToImage}"/>
-                  <p>${shortDescription}</p>
-                </a>
-              </div>`
-
-
-          //Here we create and add html elements to our html file
-					document.querySelector(".headnews").innerHTML =
-            `<div class="headnews-news">
-                <a href ="${article.url}" target="_blank">
-                  <h2>${article.title}</h2>
-                  <img src="${article.urlToImage}"/>
-                </a>
-              </div>`
-
-          }
     })
 }
 
-const recievedNewsSideBar = (newsdata) => {
+
+// Our function newsBox div
+const recievedNewsNewsBox = (newsdata) => {
 
 	// For each article object from the API, we create a new div in HTML.
     newsdata.articles.forEach((article) => {
@@ -55,24 +41,81 @@ const recievedNewsSideBar = (newsdata) => {
 
       if(article.urlToImage) {
 
-//Here we create and add html elements to our html file
-document.querySelector(".latestNewsSideBar").innerHTML +=
-  `<div class="latestNewsSideBar-news">
-      <a href ="${article.url}" target="_blank">
-        <h6>${article.publishedAt}</h6>
-        <h2>${article.title}</h2>
-      </a>
-    </div>`
-  }
-})
+				//Here we create and add html elements to our html file
+				document.querySelector(".newsBox").innerHTML +=
+          `<div class="newsBox-news">
+              <a href ="${article.url}" target="_blank">
+                <h6>${article.publishedAt}</h6>
+                <h3>${article.title}</h3>
+                <h5>${article.source.name}</h5>
+                <img src="${article.urlToImage}"/>
+                <p>${shortDescription}</p>
+              </a>
+            </div>`
+
+      }
+    })
 }
 
 
-//Fetch is a built in function in Javascript, it gets the data from the API and tranforms it into Javascript objects – JSON data.
-fetch(urldetox)
-  .then(response => response.json())
-  .then(recievedNews)
+// Our function latestNewsSideBar div
+const recievedNewsSideBar = (newsdata) => {
 
-fetch(urlDigital)
+	// For each article object from the API, we create a new div in HTML.
+    newsdata.articles.forEach((article, index) =>{
+
+      if(article.urlToImage && index < 5) {
+
+        //Here we create and add html elements to our html file
+        document.querySelector(".latestNewsSideBar").innerHTML +=
+          `<div class="latestNewsSideBar-news">
+              <a href ="${article.url}" target="_blank">
+                <h6>${article.publishedAt}</h6>
+                <h3>${article.title}</h3>
+              </a>
+            </div>`
+
+      }
+    })
+}
+
+
+// Our function popularNewsSideBar div
+const recievedpopularNewsSideBar = (newsdata) => {
+
+	// For each article object from the API, we create a new div in HTML.
+    newsdata.articles.forEach((article, index) =>{
+
+        if(index < 5)  {
+
+        //Here we create and add html elements to our html file
+        document.querySelector(".popularNewsSideBar").innerHTML +=
+          `<div class="popularNewsSideBar-news">
+              <a href ="${article.url}" target="_blank">
+                <h6>${article.publishedAt}</h6>
+                <h3>${article.title}</h3>
+              </a>
+            </div>`
+
+      }
+    })
+}
+
+
+
+//Fetch is a built in function in Javascript, it gets the data from the API and tranforms it into Javascript objects – JSON data.
+fetch(urlheadnews)
+  .then(response => response.json())
+  .then(recievedNewsheadnews)
+
+fetch(urlNewsBox)
+  .then(response => response.json())
+  .then(recievedNewsNewsBox)
+
+fetch(urlSideBar)
   .then(response => response.json())
   .then(recievedNewsSideBar)
+
+fetch(urlpopularSideBar)
+  .then(response => response.json())
+  .then(recievedpopularNewsSideBar)
